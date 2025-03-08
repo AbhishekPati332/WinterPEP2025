ae9e416bab3184c47c1ab0f06d4cbb4f6411e5af
AbhishekPati332 <abhishekpatidar332@gmail.com>
2025-03-08 19:51:59 +0530

    s

--- frontend-new/vite.config.js
+++ frontend-new/vite.config.js
@@ -1,9 +1,20 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [viteCompression()],
   build: {
-    outDir: 'dist'  // Ensure Vite outputs to 'dist'
-  }
-})
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("@mui")) return "mui"; // Separate MUI
+            if (id.includes("react")) return "react"; // Separate React
+            if (id.includes("react-router-dom")) return "router"; // Separate Router
+            return "vendor"; // Other dependencies
+          }
+        },
+      },
+    },
+  },
+});

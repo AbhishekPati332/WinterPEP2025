26538b7523deab51be1dac179665c690fdda7b13
Abhishek Patidar <117095253+AbhishekPati332@users.noreply.github.com>
2025-04-02 15:01:58 +0530

    Delete backend directory

--- backend/routes/qnaRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const qnaController = require('../controllers/qnaController');
-const auth = require('../middleware/auth');
-
-// Protected route - only authenticated users can ask questions
-router.post('/ask', auth, qnaController.askQuestion);
-
-module.exports = router; 
